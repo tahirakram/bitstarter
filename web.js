b72f69a2a6ca3515981824cfb210e525110e41f0
Tahir Akram <tahir@ubuntu.(none)>
2013-08-08 03:51:51 -0700

    web.js updated with index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('index.html');
+    var buf = fs.readFileSync('index.html');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
